9f3f1643a6eac0e79816a4695923135c52dc81dd
llsg822 <llsg822@gmail.com>
2019-07-31 00:15:32 +0900

    마이큐 설정, 튜토리얼 페이지 개선

--- app/src/main/java/com/example/whatsupq/TutorialFragment.kt
+++ app/src/main/java/com/example/whatsupq/TutorialFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.tutorial_page.*
 
-class TutorialBFragment : Fragment() {
+class TutorialFragment(tutoText: String, resId: Int) : Fragment() {
+    private val txt = tutoText
+    private val rid = resId
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.tutorial_page, container, false)
     }
@@ -15,7 +17,7 @@ class TutorialBFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        tutorial_txt.text = "혼술, 혼밥에 친환경 마스크팩도 정기배송 받아 볼 수 있어요."
-        tutorial_img.setImageResource(R.mipmap.ic_launcher)
+        tutorial_txt.text = txt
+        tutorial_img.setImageResource(rid)
     }
 }
