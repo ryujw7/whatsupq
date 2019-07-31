cdb814e80dd6e4f4879416b52b76bc17121bb413
RYUJI <rgw339@git>
2019-07-31 21:12:12 +0900

    홈 레이아웃 업데이트(모두의 선택 탭 프래그먼트 틀 완성, 생필품 탭 프래그먼트 진행중..)

--- app/src/main/java/com/example/whatsupq/ui/main/PlaceholderFragment.kt
+++ app/src/main/java/com/example/whatsupq/ui/main/PlaceholderFragment.kt
@@ -24,12 +24,10 @@ class PlaceholderFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_home, container,false)
         val homePagerAdapter = HomePagerAdapter(root.context, childFragmentManager)
         val viewPager: SwipeViewPager = root.findViewById(R.id.view_pager_home)
-        viewPager.setPagingEnabled(false)
+        viewPager.setPagingEnabled(true)
         viewPager.adapter = homePagerAdapter
         val tabs_home: TabLayout = root.findViewById(R.id.tabs)
         tabs_home.setupWithViewPager(viewPager)
-        tabs_home.setSelectedTabIndicatorGravity(TabLayout.INDICATOR_GRAVITY_BOTTOM)
-        tabs_home.setSelectedTabIndicatorColor(Color.rgb(16,106,150))
         return root
     }
 
