df3d3000f3c8c4ba304fab4404db4a05e80b618a
llsg822 <llsg822@gmail.com>
2019-09-04 03:09:48 +0900

    충돌 해결

--- app/src/main/java/com/example/whatsupq/ui/home/ChoicePlaceholderFragment.kt
+++ app/src/main/java/com/example/whatsupq/ui/home/ChoicePlaceholderFragment.kt
@@ -15,11 +15,10 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
 import com.android.volley.Response
-import com.android.volley.VolleyError
+import com.android.volley.Response.Listener
 import com.android.volley.toolbox.ImageRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
-import com.example.whatsupq.LivingItemInfoActivity
 import com.example.whatsupq.R
 import com.example.whatsupq.SwipeViewPager
 import com.example.whatsupq.ui.themebox.ThemeboxActivity
@@ -27,6 +26,7 @@ import kotlinx.android.synthetic.main.fragment_home_themebox_item.view.*
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
+import kotlin.collections.ArrayList
 
 /**
  * A placeholder fragment containing a simple view.
@@ -35,21 +35,21 @@ class ChoicePlaceholderFragment : Fragment() {
 
     private var itemList1 = ArrayList<ChoiceItem>()
     private var itemList2 = ArrayList<ChoiceItem>()
-
-    lateinit var mAdapter2 : ChoiceItemAdapter
-    lateinit var mAdapter1 : ChoiceItemAdapter
-    lateinit var mAdapter3 : HomeBannerItemAdapter
-    lateinit var themebox : View
+    private var specialList = ArrayList<String>()
+    lateinit var mAdapter2: ChoiceItemAdapter
+    lateinit var mAdapter1: ChoiceItemAdapter
+    lateinit var mAdapter3: HomeBannerItemAdapter
+    lateinit var themebox: View
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_home_choiceall, container, false)
         themebox = root.findViewById<View>(R.id.fragment_home_theme_new1)
-        if(!itemList1.isEmpty()) {
+        if (!itemList1.isEmpty()) {
             itemList1.clear()
         }
-        if(!itemList2.isEmpty()) {
+        if (!itemList2.isEmpty()) {
             itemList2.clear()
         }
         mAdapter1 = ChoiceItemAdapter(activity!! as Context, itemList1)
@@ -76,6 +76,7 @@ class ChoicePlaceholderFragment : Fragment() {
 
         return frag
     }
+
     fun getBitmap() {
         lateinit var specialJSONArray: JSONArray
         lateinit var todayJsonArray: JSONArray
@@ -83,116 +84,114 @@ class ChoicePlaceholderFragment : Fragment() {
         lateinit var themeBoxJsonObject: JSONObject
         lateinit var data: JSONObject
         lateinit var imageRequest: ImageRequest
+        lateinit var themebox_id : String
         val imgQueue = Volley.newRequestQueue(context)
         val queue = Volley.newRequestQueue(context)
         try {
             val jsonObjectRequest = JsonObjectRequest(Request.Method.GET,
                 "http://54.180.46.143:3000/api/product/main", null,
-                Response.Listener {
+                Listener {
                     var status = it.getString("status")
                     var isSuccess = it.getString("success")
                     var message = it.getString("message")
                     if (status.equals("200")) {
                         data = it.getJSONObject("data")
+                        Log.e("dataset : " , data.toString())
                         specialJSONArray = data.getJSONArray("special")
                         todayJsonArray = data.getJSONArray("today")
                         productJsonArray = data.getJSONArray("product")
                         themeBoxJsonObject = data.getJSONObject("themebox")
+                        for(i in 0 until specialJSONArray.length()) {
+                            specialList.add(specialJSONArray.getJSONObject(i).getString("main_img"))
+                        }
                         for (i in 0 until specialJSONArray.length()) {
                             try {
-                                if(mAdapter3.resIdList.isNotEmpty()) {
+                                if (mAdapter3.resIdList.isNotEmpty()) {
                                     mAdapter3.resIdList.clear()
                                 }
                                 imageRequest = ImageRequest(
-                                    specialJSONArray.getJSONObject(i).getString("main_img"),
-                                    object : Response.Listener<Bitmap> {
-                                        override fun onResponse(response: Bitmap) {
-                                            mAdapter3.resIdList.add(response)
-                                            mAdapter3.notifyDataSetChanged()
-                                        }
+                                    specialList[i],
+                                    Listener<Bitmap> { response ->
+                                        mAdapter3.resIdList.add(BannerList(i,response))
+                                        mAdapter3.sortList()
+                                        mAdapter3.notifyDataSetChanged()
                                     }, 0, 0, ImageView.ScaleType.MATRIX, Bitmap.Config.RGB_565,
-                                    object : Response.ErrorListener {
-                                        override fun onErrorResponse(error: VolleyError?) {
-                                            Toast.makeText(context, "통신 오류", Toast.LENGTH_SHORT).show()
-                                            Log.e("error", "통신 오류")
-                                        }
+                                    Response.ErrorListener {
+                                        Toast.makeText(context, "통신 오류", Toast.LENGTH_SHORT).show()
+                                        Log.e("error", "통신 오류")
                                     }
                                 )
+                                imageRequest.setShouldCache(false)
                                 imgQueue.add(imageRequest)
                             } catch (e: JSONException) {
                                 Log.d("JSON 오류 : ", "JSON이 비어있거나 삽입할 수 없음")
                             }
                         }
-                        for (i in 0 until todayJsonArray.length()) {
+                        for (j in 0 until todayJsonArray.length()) {
                             try {
                                 imageRequest = ImageRequest(
-                                    todayJsonArray.getJSONObject(i).getString("main_img"),
-                                    object : Response.Listener<Bitmap> {
-                                        override fun onResponse(response: Bitmap) {
-                                            itemList1.add(ChoiceItem(response))
-                                            mAdapter1.notifyDataSetChanged()
-
-                                        }
+                                    todayJsonArray.getJSONObject(j).getString("main_img"),
+                                    Listener<Bitmap> { response ->
+                                        itemList1.add(ChoiceItem(j, response))
+                                        sortLIst(itemList1)
+                                        mAdapter1.notifyDataSetChanged()
                                     }, 0, 0, ImageView.ScaleType.MATRIX, Bitmap.Config.RGB_565,
-                                    object : Response.ErrorListener {
-                                        override fun onErrorResponse(error: VolleyError?) {
-                                            Toast.makeText(context, "통신 오류", Toast.LENGTH_SHORT).show()
-                                            Log.e("error", "통신 오류")
-                                        }
+                                    Response.ErrorListener {
+                                        Toast.makeText(context, "통신 오류", Toast.LENGTH_SHORT).show()
+                                        Log.e("error", "통신 오류")
                                     }
                                 )
+                                imageRequest.setShouldCache(false)
                                 imgQueue.add(imageRequest)
                             } catch (e: JSONException) {
                                 Log.d("JSON 오류 : ", "JSON이 비어있거나 삽입할 수 없음")
                             }
                         }
-                        for (i in 0 until productJsonArray.length()) {
+                        for (k in 0 until productJsonArray.length()) {
                             try {
                                 imageRequest = ImageRequest(
-                                    productJsonArray.getJSONObject(i).getString("main_img"),
-                                    object : Response.Listener<Bitmap> {
-                                        override fun onResponse(response: Bitmap) {
-                                            itemList2.add(ChoiceItem(response))
-                                            mAdapter2.notifyDataSetChanged()
-                                        }
+                                    productJsonArray.getJSONObject(k).getString("main_img"),
+                                    Listener<Bitmap> { response ->
+                                        itemList2.add(ChoiceItem(k, response))
+                                        sortLIst(itemList2)
+                                        mAdapter2.notifyDataSetChanged()
                                     }, 0, 0, ImageView.ScaleType.CENTER_CROP, Bitmap.Config.RGB_565,
-                                    object : Response.ErrorListener {
-                                        override fun onErrorResponse(error: VolleyError?) {
-                                            Toast.makeText(context, "통신 오류", Toast.LENGTH_SHORT).show()
-                                            Log.e("error", "통신 오류")
-                                        }
-
+                                    Response.ErrorListener {
+                                        Toast.makeText(context, "통신 오류", Toast.LENGTH_SHORT).show()
+                                        Log.e("error", "통신 오류")
                                     }
                                 )
+                                imageRequest.setShouldCache(false)
                                 imgQueue.add(imageRequest)
                             } catch (e: JSONException) {
                                 Log.d("JSON 오류 : ", "JSON이 비어있거나 삽입할 수 없음")
                             }
                         }
                         try {
+                            themebox_id = themeBoxJsonObject.getString("themebox_id")
                             imageRequest = ImageRequest(
                                 themeBoxJsonObject.getString("main_img"),
-                                object : Response.Listener<Bitmap> {
-                                    override fun onResponse(response: Bitmap) {
-                                        val float = 12
-                                        val result = setRoundCorner(response,float.toFloat())
-                                        themebox.theme_image.scaleType = ImageView.ScaleType.CENTER_CROP
-                                        themebox.theme_image.setColorFilter(Color.parseColor("#BDBDBD"), PorterDuff.Mode.MULTIPLY)
-                                        themebox.theme_image.setImageBitmap(response)
-                                        themebox.theme_image.clipToOutline = true
-                                        themebox.theme_image.setOnClickListener {
-                                            val intent = Intent(context, ThemeboxActivity::class.java)
-                                            startActivity(intent)
-                                        }
+                                Listener<Bitmap> { response ->
+                                    val float = 12
+                                    themebox.theme_image.scaleType = ImageView.ScaleType.CENTER_CROP
+                                    themebox.theme_image.setColorFilter(
+                                        Color.parseColor("#BDBDBD"),
+                                        PorterDuff.Mode.MULTIPLY
+                                    )
+                                    themebox.theme_image.setImageBitmap(response)
+                                    themebox.theme_image.clipToOutline = true
+                                    themebox.theme_image.setOnClickListener {
+                                        val intent = Intent(context, ThemeboxActivity::class.java)
+                                        intent.putExtra("themebox_id", themebox_id)
+                                        startActivity(intent)
                                     }
                                 }, 0, 0, ImageView.ScaleType.CENTER_CROP, Bitmap.Config.RGB_565,
-                                object : Response.ErrorListener {
-                                    override fun onErrorResponse(error: VolleyError?) {
-                                        Toast.makeText(context, "통신 오류", Toast.LENGTH_SHORT).show()
-                                        Log.e("error", "통신 오류")
-                                    }
+                                Response.ErrorListener {
+                                    Toast.makeText(context, "통신 오류", Toast.LENGTH_SHORT).show()
+                                    Log.e("error", "통신 오류")
                                 }
                             )
+                            imageRequest.setShouldCache(false)
                             imgQueue.add(imageRequest)
                         } catch (e: JSONException) {
                             Log.d("JSON 오류 : ", "JSON이 비어있거나 삽입할 수 없음")
@@ -204,22 +203,28 @@ class ChoicePlaceholderFragment : Fragment() {
                     Toast.makeText(context, it.message, Toast.LENGTH_SHORT).show()
                     Log.e("error", "통신 오류")
                 })
+            jsonObjectRequest.setShouldCache(false)
             queue.add(jsonObjectRequest)
         } catch (e: JSONException) {
             Log.d("JSON 오류 : ", "JSON이 비어있거나 삽입할 수 없음")
         }
     }
-    fun setRoundCorner(bitmap: Bitmap, pixel : Float) : Bitmap {
+
+    fun selecter(choiceItem : ChoiceItem) : Int = choiceItem.index
+    fun sortLIst(itemList : ArrayList<ChoiceItem>) {
+        itemList.sortBy { selecter(it) }
+    }
+    fun setRoundCorner(bitmap: Bitmap, pixel: Float): Bitmap {
         val output = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
         val canvas = Canvas(output)
         val color = 0xff424242
         val paint = Paint()
-        val rect = Rect(0,0,bitmap.width, bitmap.height)
+        val rect = Rect(0, 0, bitmap.width, bitmap.height)
         val rectf = RectF(rect)
 
         paint.isAntiAlias = true
         paint.color = color.toInt()
-        canvas.drawARGB(0,0,0,0)
+        canvas.drawARGB(0, 0, 0, 0)
         canvas.drawRoundRect(rectf, pixel, pixel, paint)
 
         paint.setXfermode(PorterDuffXfermode(PorterDuff.Mode.SRC_IN))
